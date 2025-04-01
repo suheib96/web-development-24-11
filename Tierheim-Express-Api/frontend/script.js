156c5b8827176debaddf73ca68b094c01839c466
Your Name <your.email@example.com>
2025-04-01 13:59:00 +0200

    Veränderungen 01.04.

--- Tierheim-Express-Api/frontend/script.js
+++ Tierheim-Express-Api/frontend/script.js
@@ -2,8 +2,35 @@ const submitButton = document.getElementById("submitButton");
 const nameInput = document.getElementById("name");
 const artInput = document.getElementById("art");
 const antwortFeld = document.getElementById("antwort");
+const liste = document.getElementById("liste");
+const methodSelect = document.getElementById("method");
+const idInput = document.getElementById("nummer");
 
 submitButton.addEventListener("click", () => {
+    if (methodSelect.value == "create") {
+        createAnimal()
+    } else if (methodSelect.value == "change") {
+        changeAnimal()
+    } else if (methodSelect.value == "delete") {
+        deleteAnimal()
+    }
+})
+
+methodSelect.addEventListener("change", () => {
+    if (methodSelect.value == "create") {
+        idInput.style.display = "none";
+    } else if (methodSelect.value == "change") {
+        idInput.style.display = "block";
+    } else if (methodSelect.value == "delete") {
+        idInput.style.display = "block";
+    }
+}) 
+
+window.onload = () => {
+    refreshList()
+}
+
+function createAnimal() {
     try {
         const requestBody = {
             name: nameInput.value,
@@ -17,17 +44,53 @@ submitButton.addEventListener("click", () => {
         })  .then(res => res.json())
             .then(data => {
                 antwortFeld.innerText = JSON.stringify(data);
-                console.log(data);
+                refreshList()
+            })
+            .catch(err => {
+                console.error("Fehler beim POST:", err);
             })
     } catch (err) {
         console.log("fehler bei fetch: " + err)
     }
-})
+}
 
-window.onload = () => {
+function changeAnimal() {
+    fetch(`http://localhost:5005/tiere/${idInput.value}`, {
+        method: "PUT", 
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({art: artInput.value})
+    })
+    .then(res => res.json())
+    .then(data => {
+        antwortFeld.innerText = JSON.stringify(data)
+        refreshList()
+    })
+}
+
+function deleteAnimal() {
+    fetch(`http://localhost:5005/tiere/${Number(idInput.value)}`, {
+        method: "DELETE"
+    })
+    .then(res => res.json())
+    .then(data => {
+        antwortFeld.innerText = JSON.stringify(data)
+        refreshList()
+    })
+}
+
+function refreshList(){
+    liste.innerHTML = "";
     fetch("http://localhost:5005/tiere")
         .then(res => res.json())
         .then(data => {
-            antwortFeld.innerText = JSON.stringify(data);
+            data.forEach(element => {
+                let newListItem = document.createElement("li");
+                newListItem.innerText = `#${element.id}: ${element.name} (${element.art})`
+                liste.appendChild(newListItem)
+            });
+        })
+        .catch(err => {
+            console.error("Fehler beim refresh:", err);
         })
 }
+
