d96f2eb029955f714f599922afbf31c9c7c566b5
Suheib Marzouka <suheibmarzouka@Mac.fritz.box>
2025-04-17 11:15:10 +0200

    useState für styling genutzt

--- my-second-react-app/src/App.js
+++ my-second-react-app/src/App.js
@@ -4,14 +4,23 @@ import {useState} from "react"
 function App() {
   // let zahl = 15
   const [zahl, setZahl] = useState(15)
+  const [colour, setColour] = useState("red")
 
+  function reset(){
+    setZahl(0)
+  }
+  
   return (
    <>
    <p>Meine Lieblingszahl ist {zahl}</p>
+
    <button onClick={() => setZahl(zahl + 1) }>+</button>
    <button onClick={() => setZahl(zahl - 1) }>-</button>
-   <button onClick={() => setZahl(0) }>Reset</button>
-   
+   <button onClick={reset}>Reset</button>
+
+   <h1 style={{color: colour}}>Ich bin eine Überschrift</h1>
+   <button onClick={() => setColour("blue")}>Ändere die Farbe zu blau</button>
+   <button onClick={() => setColour("green")}>Ändere die Farbe zu grün</button>
    </>
   );
 }
