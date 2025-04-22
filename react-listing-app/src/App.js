2a7d3b388c67c9eca6530bed9b44f0157d450505
Suheib Marzouka <suheibmarzouka@Mac.fritz.box>
2025-04-22 12:08:37 +0200

    react-listing-app etwas gestyled und routing eingebaut

--- react-listing-app/src/App.js
+++ react-listing-app/src/App.js
@@ -10,10 +10,10 @@ function App() {
     {Id: 3, Name: "Mein drittes Haus", Preis: "100.000", image: "https://www.tc.de/Resources/Public/_processed_/6/9/csm_ext-einfamilienhaus-flair-134-gardenview2-2024-01-31-131100-w1600-q70_acdd41d4ac.webp"}
   ]
   return (
-    <div className="App">
+    <div className="HouseContainer">
     {houses.map((house) => 
       <Listing key={house.Id} Name={house.Name} Preis={house.Preis} image={house.image}> </Listing>
-    ) }
+    )}
 
     </div>
   );
--- react-listing-app/src/index.js
+++ react-listing-app/src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import Kontakt from "./Kontakt";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App></App>}></Route>
+        <Route path="/contact" element={<Kontakt></Kontakt>}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
