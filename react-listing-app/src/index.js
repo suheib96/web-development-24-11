4883f29edb3a88dc1ace4d055188d15c9bd19ac3
Suheib Marzouka <suheibmarzouka@Mac.fritz.box>
2025-04-23 12:51:45 +0200

    Pokemon Route ausgebaut

--- react-listing-app/src/index.js
+++ react-listing-app/src/index.js
@@ -6,6 +6,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import Kontakt from "./Kontakt";
 
+import PokemonList from "./PokemonList";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -13,6 +15,7 @@ root.render(
       <Routes>
         <Route path="/" element={<App></App>}></Route>
         <Route path="/contact" element={<Kontakt></Kontakt>}></Route>
+        <Route path="/pokemon" element={<PokemonList></PokemonList>}></Route>
       </Routes>
     </Router>
   </React.StrictMode>
