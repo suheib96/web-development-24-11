5b5cbde5263982b9c7ad2443c697fe9acf0b6dea
Suheib Marzouka <suheibmarzouka@Mac.fritz.box>
2025-04-23 10:19:12 +0200

    ternary operator hinzugefügt

--- react-listing-app/src/App.js
+++ react-listing-app/src/App.js
@@ -6,17 +6,17 @@ import { useState } from 'react';
 function App() {
 const [anzahl, setAnzahl] = useState(1);
   const houses = [
-    {Id: 1, Name: "Mein erstes Haus", Preis: "350.000", image: "https://www.hanse-haus.de/fileadmin/_processed_/8/b/csm_fertighaus-variant-25-192-hero_bc4464687c.jpg"},
-    {Id: 2, Name: "Mein zweites Haus", Preis: "125.000", image: "https://www.tc.de/Resources/Public/_processed_/6/9/csm_ext-einfamilienhaus-flair-134-gardenview2-2024-01-31-131100-w1600-q70_acdd41d4ac.webp"},
-    {Id: 3, Name: "Mein drittes Haus", Preis: "100.000", image: "https://www.tc.de/Resources/Public/_processed_/6/9/csm_ext-einfamilienhaus-flair-134-gardenview2-2024-01-31-131100-w1600-q70_acdd41d4ac.webp"},
-    {Id: 4, Name: "Mein viertes Haus", Preis: "150.000", image: "https://www.tc.de/Resources/Public/_processed_/6/9/csm_ext-einfamilienhaus-flair-134-gardenview2-2024-01-31-131100-w1600-q70_acdd41d4ac.webp"},
-    {Id: 5, Name: "Mein fuenftes Haus", Preis: "200.000", image: "https://www.tc.de/Resources/Public/_processed_/6/9/csm_ext-einfamilienhaus-flair-134-gardenview2-2024-01-31-131100-w1600-q70_acdd41d4ac.webp"},
-    {Id: 6, Name: "Mein sechstes Haus", Preis: "250.000", image: "https://www.tc.de/Resources/Public/_processed_/6/9/csm_ext-einfamilienhaus-flair-134-gardenview2-2024-01-31-131100-w1600-q70_acdd41d4ac.webp"},
+    {Id: 1, Name: "Mein erstes Haus", Preis: "350.000", image: "https://www.hanse-haus.de/fileadmin/_processed_/8/b/csm_fertighaus-variant-25-192-hero_bc4464687c.jpg",active: true,reserviert: false},
+    {Id: 2, Name: "Mein zweites Haus", Preis: "125.000", image: "https://www.tc.de/Resources/Public/_processed_/6/9/csm_ext-einfamilienhaus-flair-134-gardenview2-2024-01-31-131100-w1600-q70_acdd41d4ac.webp",active: true,reserviert: false},
+    {Id: 3, Name: "Mein drittes Haus", Preis: "100.000", image: "https://www.tc.de/Resources/Public/_processed_/6/9/csm_ext-einfamilienhaus-flair-134-gardenview2-2024-01-31-131100-w1600-q70_acdd41d4ac.webp",active: false, reserviert: true},
+    {Id: 4, Name: "Mein viertes Haus", Preis: "150.000", image: "https://www.tc.de/Resources/Public/_processed_/6/9/csm_ext-einfamilienhaus-flair-134-gardenview2-2024-01-31-131100-w1600-q70_acdd41d4ac.webp",active: false, reserviert: true},
+    {Id: 5, Name: "Mein fuenftes Haus", Preis: "200.000", image: "https://www.tc.de/Resources/Public/_processed_/6/9/csm_ext-einfamilienhaus-flair-134-gardenview2-2024-01-31-131100-w1600-q70_acdd41d4ac.webp",active: true, reserviert: false},
+    {Id: 6, Name: "Mein sechstes Haus", Preis: "250.000", image: "https://www.tc.de/Resources/Public/_processed_/6/9/csm_ext-einfamilienhaus-flair-134-gardenview2-2024-01-31-131100-w1600-q70_acdd41d4ac.webp",active: false, reserviert: true},
   ]
   return (
     <div className="HouseContainer">
     {houses.slice(0, anzahl).map((house) => 
-      <Listing key={house.Id} Name={house.Name} Preis={house.Preis} image={house.image}> </Listing>
+      <Listing key={house.Id} Name={house.Name} Preis={house.Preis} image={house.image} verfügbarkeit={house.active} reserviert={house.reserviert}> </Listing>
     )}
   <button onClick={() => setAnzahl(anzahl + 1)}>Mehr anzeigen</button>
     </div>
--- react-listing-app/src/Listing.js
+++ react-listing-app/src/Listing.js
@@ -1,12 +1,28 @@
 import { Link } from "react-router-dom";
 import "./Listing.css";
 function Listing(props) {
+  let isAdmin = true;
+  
   return (
     <div className="listing-tile">
       <img width="300px" src={props.image}></img>
       <h2>{props.Name}</h2>
       <h2>Preis: {props.Preis}€</h2>
       <Link to="/contact">Leite mich weiter auf die Kontakt Seite</Link>
+
+      {props.verfügbarkeit ? (
+        <h3 style={{ color: "green" }}>Verfügbar</h3>
+      ) : (
+        <h3 style={{ color: "red" }}>Nicht Verfügbar</h3>
+      )}
+
+      {props.reserviert ? (
+        <h4 style={{ color: "orange" }}>Reserviert</h4>
+      ) : (
+        <h4 style={{ color: "green" }}>Nicht Reserviert</h4>
+      )}
+
+      {isAdmin && <button>Delete button</button>}
     </div>
   );
 }
