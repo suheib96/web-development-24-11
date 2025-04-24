40481b516ebedac394c22a80cd60ad367a387b32
Suheib Marzouka <suheibmarzouka@Mac.fritz.box>
2025-04-24 10:16:01 +0200

    Bug in der PokemonList behoben & Pokedex Detail Seite angefangen

--- react-listing-app/src/PokemonCard.js
+++ react-listing-app/src/PokemonCard.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import './PokemonCard.css'
+import { Link } from 'react-router-dom'
 
 function PokemonCard(props) {
   return (
+    <Link style={{textDecoration: "none"}} to="/pokedex">
     <div className='container'>
-        <img src={props.image} width={"200px"}></img>
+        <img src={props.image} width={"200px"} ></img>
         <h2>Name: {props.Name}</h2>
         <p>Gewicht: {props.Gewicht}</p>
         <p>Größe: {props.Größe}</p>
         <p>Typ: {props.Typ}</p>
     </div>
+    </Link>
   )
 }
 
